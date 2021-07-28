642f08ab1be80e39d538f988e3ca43262607712d
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-07-28 16:49:41 +0300

    added header and footer, hover effects

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-$(function () {
-    
-})
+// Burger menu
+const menuBurger = document.querySelector('.menu-burger');
+// const menuList = document.querySelector('.menu__list');
+const menuButtons = document.querySelector('.menu-buttons');
+
+menuBurger.addEventListener('click', () => {
+	menuBurger.classList.toggle('menu-burger--active');
+	menuButtons.classList.toggle('menu-buttons--active');
+	// menuList.classList.toggle('menu__list--active');
+});
