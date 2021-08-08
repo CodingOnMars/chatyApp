3730f65e7b34130ba239ea2dccd9adfb6ff9b5fb
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-08-08 11:34:55 +0300

    style tweaks and fixes, refactoring

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,21 @@
 // Burger menu
-const menuBurger = document.querySelector('.menu-burger');
-// const menuList = document.querySelector('.menu__list');
-const menuButtons = document.querySelector('.menu-buttons');
 
-menuBurger.addEventListener('click', () => {
-	menuBurger.classList.toggle('menu-burger--active');
-	menuButtons.classList.toggle('menu-buttons--active');
-	// menuList.classList.toggle('menu__list--active');
+$('.menu-burger').on('click', function () {
+	$('.menu-burger, .menu, .menu-buttons').toggleClass('active');
+	$('body').toggleClass('lock');
+});
+
+// Sticky scroll header
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+	header.classList.toggle('sticky', scrollY > 0);
 });
 
 //////////////////////////////////////////////////////////////////////////////////
 
 // Authorisation
 // Common classes
+const menuBurger = document.querySelector('.menu-burger');
 const formInputs = document.querySelectorAll('.form__input');
 const formErrors = document.querySelectorAll('.form__error');
 const overlay = document.querySelector('.overlay');
