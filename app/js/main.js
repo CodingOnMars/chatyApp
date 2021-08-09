84f84605c0432f6840adbbe2f95025b5db7e5066
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-08-09 20:46:24 +0300

    new form validation, style tweaks, refactoring

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,4 @@
 // Burger menu
-
 $('.menu-burger').on('click', function () {
 	$('.menu-burger, .menu, .menu-buttons').toggleClass('active');
 	$('body').toggleClass('lock');
@@ -16,7 +15,7 @@ window.addEventListener('scroll', () => {
 // Authorisation
 // Common classes
 const menuBurger = document.querySelector('.menu-burger');
-const formInputs = document.querySelectorAll('.form__input');
+const formFields = document.querySelectorAll('.form__field');
 const formErrors = document.querySelectorAll('.form__error');
 const overlay = document.querySelector('.overlay');
 
@@ -76,33 +75,14 @@ overlay.addEventListener('click', () => {
 const loginForm = document.querySelector('.login-form');
 const loginPhone = document.querySelector('#login-phone');
 const loginPassword = document.querySelector('#login-password');
-const phoneError = document.querySelector('#login-phone-error');
-const passwordError = document.querySelector('#login-password-error');
 
 loginForm.addEventListener('submit', e => {
-	clearErrors();
+	let isPhoneValid = checkPhone(loginPhone);
+	isPasswordValid = checkLoginPassword(loginPassword);
 
-	let errorMessages = [];
+	let isFormValid = isPhoneValid && isPasswordValid;
 
-	if (loginPhone.value < 11) {
-		errorMessages.push('Phone error');
-		phoneError.classList.add('form__error--active');
-		loginPhone.classList.add('form__input--error');
-	} else {
-		phoneError.classList.remove('form__error--active');
-		loginPhone.classList.remove('form__input--error');
-	}
-
-	if (loginPassword.value < 1) {
-		errorMessages.push('Password error');
-		passwordError.classList.add('form__error--active');
-		loginPassword.classList.add('form__input--error');
-	} else {
-		passwordError.classList.remove('form__error--active');
-		loginPassword.classList.remove('form__input--error');
-	}
-
-	if (errorMessages.length > 0) {
+	if (!isFormValid) {
 		e.preventDefault();
 	}
 });
@@ -115,64 +95,167 @@ const regPassword = document.querySelector('#reg-password');
 const regPasswordConfirmation = document.querySelector(
 	'#reg-password-confirmation'
 );
-const regPhoneError = document.querySelector('#reg-phone-error');
-const regEmailError = document.querySelector('#reg-email-error');
-const regPasswordError = document.querySelector('#reg-password-error');
-const regPasswordConfirmationError = document.querySelector(
-	'#reg-password-confirmation-error'
-);
 
 regForm.addEventListener('submit', e => {
-	clearErrors();
+	let isPhoneValid = checkPhone(regPhone),
+		isEmailValid = checkEmail(),
+		isPasswordValid = checkPassword(regPassword),
+		isConfirmPasswordValid = checkConfirmPassowrd();
 
-	let errorMessages = [];
+	let isFormValid =
+		isPhoneValid && isEmailValid && isPasswordValid && isConfirmPasswordValid;
 
-	if (regPhone.value < 11) {
-		errorMessages.push('Phone error');
-		regPhoneError.classList.add('form__error--active');
-		regPhone.classList.add('form__input--error');
-	} else {
-		regPhoneError.classList.remove('form__error--active');
-		regPhone.classList.remove('form__input--error');
-	}
-
-	if (regEmail.value < 1) {
-		errorMessages.push('Email error');
-		regEmailError.classList.add('form__error--active');
-		regEmail.classList.add('form__input--error');
-	} else {
-		emailError.classList.remove('form__error--active');
-		regEmail.classList.remove('form__input--error');
-	}
-
-	if (regPassword.value < 1) {
-		errorMessages.push('Password error');
-		regPasswordError.classList.add('form__error--active');
-		regPassword.classList.add('form__input--error');
-	} else {
-		regPasswordError.classList.remove('form__error--active');
-		regPassword.classList.remove('form__input--error');
-	}
-
-	if (regPasswordConfirmation.value !== regPassword.value) {
-		errorMessages.push('Passwords do not match');
-		regPasswordConfirmationError.classList.add('form__error--active');
-		regPasswordConfirmation.classList.add('form__input--error');
-	}
-
-	if (errorMessages.length > 0) {
+	if (!isFormValid) {
 		e.preventDefault();
 	}
 });
 
+// Input validation funtions
+
+// Check if entered value is empty
+function isRequired(value) {
+	return value === '' ? false : true;
+}
+
+// Check if phone input is following the pattern
+function isPhoneCorrect(phone) {
+	const re =
+		/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+	return re.test(phone);
+}
+
+// Check if password contains at least 8 characters. including at least one lower case letter, one uppercase letter, one number, one symbol
+function isPasswordSecure(password) {
+	const re = new RegExp(
+		'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})'
+	);
+	return re.test(password);
+}
+
+// Show error message, highligh input field with red color
+function showError(input, message) {
+	const formField = input.parentElement;
+	const errorMessage = formField.querySelector('small');
+
+	formField.classList.remove('valid');
+	formField.classList.add('error');
+
+	errorMessage.classList.add('form__error--active');
+	errorMessage.textContent = message;
+}
+
+// Remove error message, highligh input field with green color
+function showValid(input) {
+	const formField = input.parentElement;
+	const errorMessage = formField.querySelector('small');
+
+	formField.classList.remove('error');
+	formField.classList.add('valid');
+
+	errorMessage.classList.remove('form__error--active');
+	errorMessage.textContent = '';
+}
+
+// Validate phone input
+function checkPhone(phone) {
+	let valid = false;
+
+	const phoneInput = phone.value.trim();
+
+	if (!isRequired(phoneInput)) {
+		showError(phone, 'Введите номер телефона');
+	} else if (!isPhoneCorrect(phoneInput)) {
+		showError(phone, 'Введите номер в формате +7(ххх)-ххх-хх-хх');
+	} else {
+		showValid(phone);
+		valid = true;
+	}
+
+	return valid;
+}
+
+// Validate login password input
+function checkLoginPassword() {
+	let valid = false;
+
+	let loginPasswordInput = loginPassword.value.trim();
+
+	if (!isRequired(loginPasswordInput)) {
+		showError(loginPassword, 'Введите пароль');
+	} else {
+		showValid(loginPassword);
+		valid = true;
+	}
+
+	return valid;
+}
+
+// Validate email input for registartion form
+function checkEmail() {
+	let valid = false;
+
+	const email = regEmail.value.trim();
+
+	if (!isRequired(email)) {
+		showError(regEmail, 'Введите email');
+	} else {
+		showValid(regEmail);
+		valid = true;
+	}
+
+	return valid;
+}
+
+// Validate password input
+function checkPassword(password) {
+	let valid = false;
+
+	const passwordInput = password.value.trim();
+
+	if (!isRequired(passwordInput)) {
+		showError(password, 'Введите пароль');
+	} else if (!isPasswordSecure(passwordInput)) {
+		showError(
+			password,
+			'Пароль должен быть не менее 8 символов и содержать комбинацию букв разного регистра, цифр и символов (!#$%&?)'
+		);
+	} else {
+		showValid(password);
+		valid = true;
+	}
+
+	return valid;
+}
+
+// Validate password confirmation for registartion form
+function checkConfirmPassowrd() {
+	let valid = false;
+
+	const confirmPassword = regPasswordConfirmation.value.trim();
+	const password = regPassword.value.trim();
+
+	if (!isRequired(confirmPassword)) {
+		showError(regPasswordConfirmation, 'Пожалуйста, повторите пароль');
+	} else if (confirmPassword !== password) {
+		showError(regPasswordConfirmation, 'Введенные пароли не совпадают');
+	} else {
+		showValid(regPasswordConfirmation);
+		valid = true;
+	}
+
+	return valid;
+}
+
+// Clear error messages, remove input highlight
 function clearErrors() {
 	formErrors.forEach(el => {
 		el.classList.remove('form__error--active');
 	});
-	formInputs.forEach(el => {
-		el.classList.remove('form__input--error');
+	formFields.forEach(el => {
+		el.classList.remove('error');
 	});
 }
+
 //////////////////////////////////////////////////////////////////////////////////
 
 // Slider
